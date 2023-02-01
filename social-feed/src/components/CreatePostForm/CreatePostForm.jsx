d43741256f893e657b8da3dbe5ea5e76a8520725
Mark S <stcoronawraps@gmail.com>
2023-01-31 18:13:20 -0700

    edit:createpost

--- social-feed/src/components/CreatePostForm/CreatePostForm.jsx
+++ social-feed/src/components/CreatePostForm/CreatePostForm.jsx
@@ -1,26 +1,36 @@
-import React, { useState } from 'react';
- 
-const CreatePost = props => {
-    const [post, setPost] = useState('');
+import React, { useState } from "react";
 
-    const handlePost = event => {
-        event.preventDefault();
-        setPost(event.target.value)
-    }
-    return ( 
-        <form onSubmit={handlePost}>
-            <div>
-                <label>Posts</label>
-                <input type="" 
-                    value={post}
-                    onChange={(event) =>setPost(event.target.value)}
-                />
-            </div>
-            <button type='submit'>POST</button>
-        </form>
-            );
-    }
-  
-    <form action=""></form>
-    export default CreatePost;pro
-    
+const CreatePost = (props) => {
+  const [author, setAuthor] = useState("");
+  const [post, setPost] = useState("");
+
+  const handlePost = (event) => {
+    event.preventDefault();
+    setPost(event.target.value);
+  };
+  return (
+    <form onSubmit={handlePost}>
+      <div>
+        <label>Author</label>
+        <input
+          type=""
+          value={author}
+          onChange={(event) => setAuthor(event.target.value)}
+        />
+      </div>
+      <div>
+        <label>Posts</label>
+        <input
+          type=""
+          value={post}
+          onChange={(event) => setPost(event.target.value)}
+        />
+      </div>
+      <button type="submit">POST</button>
+    </form>
+  );
+};
+
+<form action=""></form>;
+export default CreatePost;
+pro;
